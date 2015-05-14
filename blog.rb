2c5cdb9c5ce6fdb308f3ca05d8aeadef40bb55ae
Amie Wilt <amie.wilt@gmail.com>
2015-05-14 09:14:28 -0400

    changed file path to YAML.load_file, fixed index.html.erb

--- blog.rb
+++ blog.rb
@@ -8,17 +8,10 @@ server = WEBrick::HTTPServer.new(:Port => 8000, :DocumentRoot => "#{ROOT}/public
 
 server.mount_proc '/post' do |req, res|
   @page_title = "Amie's Blog."
-  @blog = File.read("#{ROOT}/post.yml").split("\n")
+  @blog = YAML.load_file("#{ROOT}/post.yml")
   template = ERB.new(File.read("#{ROOT}/index.html.erb"))
   res.body = template.result
 end
-
-# server.mount_proc '/movies' do |req, res|
-#   @page_title = "Movies."
-#   @ice_cream = File.read("#{ROOT}/movies.txt").split("\n")
-#   template = ERB.new(File.read("#{ROOT}/index.html.erb"))
-#   res.body = template.result
-# end
 
 trap 'INT' do
   server.shutdown
